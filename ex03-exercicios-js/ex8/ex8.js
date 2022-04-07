0cf83a0991db330c11957c086a696620dcdd8122
luizgnclvs <luizgnclvss@gmail.com>
2022-04-07 02:37:44 -0300

    oitavo js consertado

--- ex03-exercicios-js/ex8/ex8.js
+++ ex03-exercicios-js/ex8/ex8.js
@@ -10,30 +10,44 @@ function cleanWallet() {
     }
 }
 
+function warning() {
+    alert("Atente-se ao fato de que, caso inseridas mais que duas casas decimais, o valor será arredondado para o número com dois decimais mais próximo");
+}
+
 function printBills() {
-    let money = parseFloat(inputMoney.value);
+    let money = (parseFloat(inputMoney.value)).toFixed(2);
     cleanWallet();
     
     textBody.innerHTML = "";
-    textBody.innerHTML += `<b>Valor inserido:</b> R$ ${money}\n\n`
+    textBody.innerHTML += `<span style="font-size: large;"><b>Valor inserido:</b> R$ ${money}</span>\n\n`
 
     for (let i = 0; i < wallet.length; i++) {
-        if (money >= wallet[i][0]) {
-            wallet[i][1] = (Math.floor(money / wallet[i][0]));
+        if (i == wallet.length - 1) {
+            money = Math.round(money * 100) / 100;
+            wallet[i][1] = money / wallet[i][0];
+        } else if (money >= wallet[i][0]) {
+            wallet[i][1] = Math.floor(money / wallet[i][0]);
             money = money % wallet[i][0];
         }
+        console.log(money, wallet[i][0], wallet[i][1]);
     }
 
     for (let i = 0; i < wallet.length; i++) {
         if (i < 7) {
-            textBody.innerHTML += `Notas de ${wallet[i][0]} reais: <b>${wallet[i][1]}</b>\n`
+            textBody.innerHTML += `Notas de R$ ${wallet[i][0].toFixed(2)} reais: <b>${wallet[i][1]}</b>\n`
+        } else if (i == 7) {
+            textBody.innerHTML += `Moedas de R$ ${wallet[i][0].toFixed(2)} real: <b>${wallet[i][1]}</b>\n`
+        } else if (i < 12) {
+            textBody.innerHTML += `Moedas de R$ ${wallet[i][0].toFixed(2)} centavos: <b>${wallet[i][1]}</b>\n`
         } else {
-            textBody.innerHTML += `Moedas de ${wallet[i][0]} centavos: <b>${wallet[i][1]}</b>\n`
+            textBody.innerHTML += `Moedas de R$ ${wallet[i][0].toFixed(2)} centavo: <b>${wallet[i][1]}</b>\n`
         }
-    }    
+    }
 
+    textBody.innerHTML += "<hr>",
     inputMoney.value = "";
     inputMoney.focus();
 }
 
 btPrint.onclick = printBills;
+inputMoney.onclick = warning;
